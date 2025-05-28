67ba5a2be0f3e40ed69f694bf4d50a717a5ba6df
rexiepimentelMBP <rexiepimentelMBP@rexies-mbp.mshome.net>
2025-05-28 13:30:08 +0800

    First Commit!

---
+++ src/pkgfinal/shape/calculator/FinalShapeCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package pkgfinal.shape.calculator;
+
+
+public class FinalShapeCalculator {
+
+    public static void main(String[] args) {
+        
+        Main main = new Main();
+        main.setVisible(true);
+        main.setSize(1150, 670);
+        main.setLocation(250, 160);
+    }
+
+}
